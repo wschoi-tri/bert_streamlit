431e322fc9d79951b89a791a48cff24d4adc0f1b
wschoi-tri <39798559+wschoi-tri@users.noreply.github.com>
2026-02-09 14:46:32 +0900

    Update streamlit_app_klue.py

--- streamlit_app_klue.py
+++ streamlit_app_klue.py
@@ -242,7 +242,7 @@ def visualize_vector_patterns(results, top_n=100):
 
 def main():
     st.set_page_config(page_title="BERT 조회", layout="centered")
-    st.title("BERT 조회 (모델선택)")
+    st.title("BERT 조회")
 
     # 모델 선택
     model_options = {
