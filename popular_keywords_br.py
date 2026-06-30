117d9666b037730c32d878a6da22228b3ae9f279
wschoi-tri <39798559+wschoi-tri@users.noreply.github.com>
2026-06-30 09:41:02 +0900

    Update popular_keywords_br.py

--- popular_keywords_br.py
+++ popular_keywords_br.py
@@ -45,7 +45,7 @@ st.markdown("""
     }
     .product-grid {
         display: grid;
-        grid-template-columns: repeat(12, 132px);
+        grid-template-columns: repeat(14, 132px);
         justify-content: center;
         gap: 7px;
         width: 100%;
